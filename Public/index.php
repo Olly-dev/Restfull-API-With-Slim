626d3c45d914f7fe1d54a75fecd038873db3b616
alhkad <alhkad.2007@yahoo.fr>
2019-11-05 10:24:31 +0100

    test avec Post et postman et un deuxième test avec get et arguments

--- Public/index.php
+++ Public/index.php
@@ -2,15 +2,43 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use Slim\Exception\NotFoundException;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// Instantiate App
 $app = AppFactory::create();
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+// Add error middleware
+//$app->addErrorMiddleware(true, true, true);
+
+// Add routes
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('<a href="/hello/world">Try /hello/world</a>');
+    return $response;
+});
+
+$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello World, $name");
+    return $response;
+});
+
+//PST TEST (tester avec Postman)
+$app->post('/testPost/demo', function(Request $req, Response $rep){
+
+	$data = $req->getParsedBody();
+	$inputdata=[];
+	$name = $inputdata['name'] = filter_var($data['name'], FILTER_SANITIZE_STRING);
+	$phone = $inputdata['phone'] = filter_var($data['phone'], FILTER_SANITIZE_STRING);
+	$rep->getBody()->write("dear $name, your phone number is $phone");
+	return $rep;
+});
+
+//Test avec arguments
+$app->get('/testargs/{name}/{phone}', function ($request, $response, $args) {
+    $name = $args['name'];
+    $phone = $args['phone'];
+    $response->getBody()->write("Hello $name This is test for args, your phone is $phone");
     return $response;
 });
 
