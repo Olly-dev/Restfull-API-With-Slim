56728275b581e6531c9a8ffd7a5c656231cd4480
alhkad <alhkad.2007@yahoo.fr>
2019-11-05 14:13:22 +0100

    Put Resource and DELETE Resource

--- Public/index.php
+++ Public/index.php
@@ -56,4 +56,26 @@ $app->get('/jsontest/{FirstName}/{LatsName}', function ($request, $response, $ar
     return $response;
 });
 
+//Put Resource
+$app->put('/testput', function($Request, $Response){
+
+	$data = $Request->getParsedBody();
+	$username = $data['UserName'];
+	$password = $data['Password'];
+	$Response->getBody()->write("$username your Password is $password");
+	return $Response;
+
+});
+
+//Delete Resource
+$app->delete('/testdelete', function($Request, $Response){
+
+	$data = $Request->getParsedBody();
+	$username = $data['UserName'];
+	$password = $data['Password'];
+	$Response->getBody()->write("$username your Password is $password With Delete Test Demo");
+	return $Response;
+
+});
+
 $app->run();
