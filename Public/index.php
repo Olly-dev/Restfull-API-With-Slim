7cfdc195a72b8e0c832c86c7079259777dae93d7
alhkad <alhkad.2007@yahoo.fr>
2019-11-05 14:39:58 +0100

    Multiple Methods

--- Public/index.php
+++ Public/index.php
@@ -78,4 +78,19 @@ $app->delete('/testdelete', function($Request, $Response){
 
 });
 
+//Multiple Methods
+$app->map(['PUT', 'GET'], '/multipleMethodsTest/{id}', function($request, $response, $args){
+
+	$id = $args['id'];
+	if($request->getMethod() == "PUT"){
+
+		$response->getBody()->write("This id=$id Will be Updated");
+	}
+	if($request->getMethod() == "GET"){
+
+		$response->getBody()->write("This id=$id Will be retrived ");
+	}
+	return $response;
+});
+
 $app->run();
