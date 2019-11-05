fccbe82e88f77b6cc66a8b0e82781f8fe9e626e6
alhkad <alhkad.2007@yahoo.fr>
2019-11-05 10:46:52 +0100

    Envoi Json Response

--- Public/index.php
+++ Public/index.php
@@ -42,4 +42,18 @@ $app->get('/testargs/{name}/{phone}', function ($request, $response, $args) {
     return $response;
 });
 
+//Test Envoi du Json Response
+$app->get('/jsontest/{FirstName}/{LatsName}', function ($request, $response, $args) {
+    $FirstName = $args['FirstName'];
+    $LatsName = $args['LatsName'];
+    $out=[];
+    $out['Status'] = 200;
+    $out['Message'] = "This is Json Response Test";
+    $out['FirstName'] = $FirstName;
+    $out['LatsName'] = $LatsName;
+
+    $response->getBody()->write(json_encode($out));
+    return $response;
+});
+
 $app->run();
