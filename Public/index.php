f878200edec59dc686c808fd603cd663f898a871
alhkad <alhkad.2007@yahoo.fr>
2019-11-05 16:19:18 +0100

    Single, Multiple and Unlimiter Optional Segments

--- Public/index.php
+++ Public/index.php
@@ -93,4 +93,52 @@ $app->map(['PUT', 'GET'], '/multipleMethodsTest/{id}', function($request, $respo
 	return $response;
 });
 
+// Optional Segments
+$app->get('/optional[/{id}]', function($request, $response, $args){
+
+	$id = $args['id'];
+	if(is_null($id)){
+		$response->getBody()->write("This id is null");
+	}
+	else{
+		$response->getBody()->write("This id = $id");
+	}
+	return $response;
+});
+
+// Multiple Optional Segments
+$app->get('/multiple/optional[/{year}[/{month}]]', function($request, $response, $args){
+
+	$year = $args['year'];
+	$month = $args['month'];
+	if(is_null($year)){
+		$response->getBody()->write("This year and month are null");
+	}
+	else{
+		if(is_null($month)){
+			$response->getBody()->write("This year = $year and the month is null");
+		}else{
+			$response->getBody()->write("This year = $year and the month = $month");
+		}
+	}
+	return $response;
+});
+
+// Unlimited Optional Segments
+$app->get('/unlimited/optional[/{params:.*}]', function($request, $response, $args){
+
+	$params = explode('/', $request->getAttribute('params'));
+	if(empty($params[0])){
+		$response->getBody()->write("The params list is null");
+	}
+	else{
+		$out="";
+		foreach ($params as $key => $value) {
+			$out = $out . " " . "$key => $value \r";
+		}
+		$response->getBody()->write($out);
+	}
+	return $response;
+});
+
 $app->run();
